ab42467ae8b5c4c498dd8a41a4d0b44cf4150aec
Shaikh Mamun Hoque <2017ucs0111@iitjammu.ac.in>
2020-04-07 18:22:55 +0530

    Update subset.c

--- subset.c
+++ subset.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <stdbool.h> 
 
-
-int isSubsetSum(int set[], int n, int sum)
+bool isSubsetSum(int set[], int n, int sum)
 {
-	// Base Cases 
+
 	if (sum == 0)
 		return 1;
 	if (n == 0 && sum != 0)
 		return 0;
 
-	// If last element is greater than sum, then ignore it 
 	if (set[n - 1] > sum)
 		return isSubsetSum(set, n - 1, sum);
 
-	/* else, check if sum can be obtained by any of the following
-		(a) including the last element
-		(b) excluding the last element */
-#pragma omp task
-	int a = isSubsetSum(set, n - 1, sum);
-#pragma omp task
-	int b = isSubsetSum(set, n - 1, sum - set[n - 1]);
-#pragma omp taskwait
+	bool a = false, b = false;
+	#pragma omp task default(shared)
+	{
+		a = isSubsetSum(set, n - 1, sum);
+	}
+	
 
-	return (a + b);
+	#pragma omp task default(shared)
+	{
+		b = isSubsetSum(set, n - 1, sum - set[n - 1]);
+	}
+	
+
+	#pragma omp taskwait
+
+	return a||b;
 
 }
 
-// Driver program to test above function 
+
 int main()
 {
 	int set[] = { 3, 34, 4, 12, 5, 2 };
 	int sum = 9;
 	int n = sizeof(set) / sizeof(set[0]);
+	bool result = false;
 
-#pragma omp parallel
-#pragma omp single
-	int result = isSubsetSum(set, n, sum);
+	#pragma omp parallel 
+	{
+		#pragma omp single
+		{
+			result = isSubsetSum(set, n, sum);
+		}
+		
+	}
 
 	if (result)
-		printf("Found a subset with given sum");
+		printf("Found a subset with given sum\n");
 	else
-		printf("No subset with given sum");
+		printf("No subset with given sum\n");
 	return 0;
 }
